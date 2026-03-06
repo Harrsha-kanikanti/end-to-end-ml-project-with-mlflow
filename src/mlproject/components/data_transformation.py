4a6a068978a14ff98565d750fd50a41a190252d6
Harrsha kaniaknti <saiiharrshakanikanti@gmail.com>
2026-03-06 20:08:56 +0530

    data transformation added

---
+++ src/mlproject/components/data_transformation.py
@@ -0,0 +1,24 @@
+from mlproject.entity.config_entity import DataTransformationConfig
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import os
+from mlproject import logger
+
+class DataTransformation:
+    def __init__(self,config:DataTransformationConfig):
+        self.config = config
+
+    def train_test_spliting(self):
+        data = pd.read_csv(self.config.data_path)
+
+        train, test = train_test_split(data)
+
+        train.to_csv(os.path.join(self.config.root_dir,"train.csv"),index=False)
+        test.to_csv(os.path.join(self.config.root_dir,"test.csv"),index=False)
+
+        logger.info("splitted data into training and test sets")
+        logger.info(train.shape)
+        logger.info(test.shape)
+
+        print(train.shape)
+        print(test.shape)
---
+++ src/mlproject/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,24 @@
+from mlproject import logger
+from mlproject.config.configuration import ConfigurationManager
+from mlproject.components.data_transformation import DataTransformation
+
+STAGE_NAME = "Data Transformation Stage"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation()
+        data_transformation = DataTransformation(config = data_transformation_config)
+        data_transformation.train_test_spliting()
+
+if __name__ == "__main__":
+    try:
+        logger.info(f"stage {STAGE_NAME} started")
+        obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f"stage {STAGE_NAME} completed")
+    except Exception as e:
+        logger.exception(e)
+        raise e
